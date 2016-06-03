1da78fde0007a87939c58b8a0277b91838746abb
June-register <examplea110@qq.com>
2016-06-04 01:30:29 +0800

    初始化

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+	var Swiper1 = new Swiper('.swiper-container1',{
+		direction : 'vertical',
+		loop : true,
+		autoplay: 1000,
+	})
+	var Swiper2 = new Swiper('.swiper-container2',{
+		pagination : '.swiper-pagination',
+		autoplay: 1000,
+		loop : true,
+	})
+})
